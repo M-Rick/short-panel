80ce264cfb918cb585fff1e7ed39acccb1b5af17
M-Rick <m.rick.mac@gmail.com>
2024-09-29 17:03:44 +0200

    Update extension.js
    Added resolution settings

--- short-panel@yourdomain.com/extension.js
+++ short-panel@yourdomain.com/extension.js
@@ -1,6 +1,7 @@
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as Panel from 'resource:///org/gnome/shell/ui/panel.js';
+import St from 'gi://St';
 import GLib from 'gi://GLib';
 
 export default class ShortPanelExtension extends Extension {
@@ -10,12 +11,7 @@ export default class ShortPanelExtension extends Extension {
         // Vérification si c'est X.org
         if (this._isX11()) {
             this._adjustPanelWidth(); // Ajuster la taille du panneau
-            this._setPanelHeight(32); // Ajuster la hauteur du panneau
-
-            // Positionner l'horloge avec l'offset uniquement
-            this._setClockMenuPosition(2, 4); // position (2 gauche, 1 centre, 0 droite), offset
-            this._setPanelIconSize(14); // Ajuster la taille des icônes
-            this._setPanelButtonPadding(4); // Ajuster le padding des boutons du panneau
+            this._setPanelHeight(30); // Ajuster la hauteur du panneau
         }
     }
 
@@ -27,32 +23,32 @@ export default class ShortPanelExtension extends Extension {
             this._resetPanelWidth();
         }
     }
-
-    _adjustPanelWidth() {
+        
+        _adjustPanelWidth() {
         const screenWidth = Main.layoutManager.primaryMonitor.width;
-        //let fixedWidth;
-        let floatingWidth;
+        let panelWidth;
 
         // Ajuster la largeur en fonction de la taille de l'écran
+        // Pour les écrans de plus de moins 1440px de large pour un scaling de 100 %
         if (screenWidth <= 1440) {
-            //fixedWidth = 420; // Largeur fixe de 420 pixels pour les écrans <= 1440px
-            floatingWidth = screenWidth * 0.25;  // 25% de la largeur de l'écran
+            //panelWidth = 420; // Largeur fixe de 420 pixels
+            panelWidth = screenWidth * 0.26;  // 26% de la largeur de l'écran
         } else {
-            //fixedWidth = 480; // Largeur fixe de 480 pixels pour les écrans > 1440px
-            floatingWidth = screenWidth * 0.3;  // 30% de la largeur de l'écran
+            // Pour les écrans de plus de 1440px de large
+            //panelWidth = 480 * scalingFactor; // Largeur fixe de 462 px pour un scaling de 100 %
+            panelWidth = 700; // Largeur de 700 px pour un scaling de 200 %
+            //panelWidth = screenWidth * 0.23;  // 23% de la largeur de l'écran pour un scaling de 100 %
+            //panelWidth = screenWidth * 0.25;  // 25% de la largeur de l'écran pour un scaling de 200 %
         }
 
         //const xPosition = screenWidth - fixedWidth; // Aligner à droite
-        const xPosition = screenWidth - floatingWidth; // Aligner à droite
+        const xPosition = screenWidth - panelWidth; // Aligner à droite
         const yPosition = 0; // Pas de décalage vertical
-
-
 
         // Appliquer la nouvelle taille et position sans animation
         Main.layoutManager.panelBox.translation_x = xPosition;
         Main.layoutManager.panelBox.translation_y = yPosition; // Pas de décalage vertical
-        //Main.layoutManager.panelBox.width = fixedWidth;
-        Main.layoutManager.panelBox.width = floatingWidth;
+        Main.layoutManager.panelBox.width = panelWidth;
     }
 
     _resetPanelWidth() {
@@ -63,53 +59,10 @@ export default class ShortPanelExtension extends Extension {
         Main.layoutManager.panelBox.translation_y = 0; // Pas de décalage vertical
         Main.layoutManager.panelBox.width = screenWidth;
     }
-
-    _setClockMenuPosition(position, offset) {
-        const clockMenu = Main.panel.statusArea.dateMenu;
-
-        // Déplacement de l'horloge selon la position
-        if (position === 2) {
-            Main.panel._leftBox.insert_child_at_index(clockMenu.container, 0);
-        } else if (position === 1) {
-            Main.panel._centerBox.insert_child_at_index(clockMenu.container, 0);
-        } else {
-            Main.panel._rightBox.insert_child_at_index(clockMenu.container, 0);
-        }
-
-        // Appliquer l'offset de positionnement
-        clockMenu.container.translation_x = offset;
-    }
     
     _setPanelHeight(size) {
         // Ajuster la hauteur du panneau
         Main.layoutManager.panelBox.height = size;
-    }
-
-    _setPanelIconSize(iconSize) {
-        // Vérifie et ajuste la taille des icônes dans les éléments du panneau
-        for (let area in Main.panel.statusArea) {
-            const item = Main.panel.statusArea[area];
-            if (item.container) {
-                item.container.get_children().forEach(child => {
-                    if (child.icon_size) {
-                        child.icon_size = iconSize; // Ajuste la taille des icônes
-                    }
-                });
-            }
-        }
-    }
-
-    _setPanelButtonPadding(paddingSize) {
-        const aggregateMenu = Main.panel.statusArea.aggregateMenu;
-
-        // Vérification que aggregateMenu existe avant d'ajuster le padding des boutons
-        if (aggregateMenu) {
-            aggregateMenu.actor.get_style_class_name = () => {
-                return `panel-button padding-${paddingSize}`;
-            };
-        } else {
-            log('aggregateMenu is undefined, skipping button padding adjustment.');
-        }
     }
     
 
